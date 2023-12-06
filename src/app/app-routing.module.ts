2745ef111539af71ab79db5eebacf1c967f29dcb
osama <oussamaknouz2000@gmail.com>
2023-12-06 20:52:36 +0100

    project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieDetailsResolver } from './movie.resolver';
+import { FavoritesMoviesComponent } from './favorites-movies/favorites-movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'movie/:id',
+    component: MovieDetailsComponent,
+    resolve: {
+      movie: MovieDetailsResolver,
+    },
+  },
+  {
+    path: 'favorites',
+    component: FavoritesMoviesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
+import { FormsModule } from '@angular/forms';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { FavoritesMoviesComponent } from './favorites-movies/favorites-movies.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieSearchComponent,
+    MovieDetailsComponent,
+    FavoritesMoviesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
